d0902a965305c6d99a32d93d5ae7f65733094462
Vlad <vovaturcan4@gmail.com>
2019-01-16 00:51:07 +0200

    done the mvp and stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+var me = "Vlad";
+function sayHello() {
+  console.log("Hello " + me);
+}
+sayHello();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => ++count;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
+  let count = 0;
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return {
+    increment: () => ++count,
+    decrement: () => (count = count - 1)
+  };
 };
+
+const test = counterFactory();
+console.log(test.increment()); // 1
+console.log(test.increment()); // 2
+console.log(test.increment()); // 3
+console.log(test.decrement()); // 2
